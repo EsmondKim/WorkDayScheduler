a1a4ecabe8017433bdde9a9a3069662344afc118
EsmondKim <ekelectronicmail@gmail.com>
2020-11-01 17:08:16 -0600

    Trying to save text from 4 pm block to local storage.

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
     console.log(moment().format());
     console.log(moment().hours());
     let currentHour = (moment().hours());
-//Header element to add day, date and time.
+    let sixteenText = $("#blockHourVariable16").val();
+    let sixteenAppointment = ("sixteen", sixteenText);
+  
+    //Header element to add day, date and time.
     $("#currentDayAndTime").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
     //The Function that changes blocks to the appropriate color
-   
     function hourReady() {  
         //for each loops
         $(".time-block").each(function() {            
             let blockHour = parseInt($(this).attr("id"));
             let blockHourVariable = "#blockHourVariable" + parseInt($(this).attr("id"));
-            console.log(blockHourVariable);
             if(blockHour < currentHour) {
                 $(blockHourVariable).addClass("past");
             }
@@ -28,13 +29,20 @@ $(document).ready(function() {
         })
     } hourReady()
 
+    //Function to write appointments to local memory.
+    $("#nineButton").click(function() {
+        $(textarea.blockHourVariable16).text(".blockHourVariable16")
+        window.localStorage.setItem(nineAppointment, JSON.stringify("blockHourVariable9").text)
+    })
+
+    $("#sixteenButton").on("click", function() {
+        console.log("test");
+        window.localStorage.setItem("sixteen", JSON.stringify(sixteenAppointment));
+        console.log(sixteenText)
+        console.log(sixteenAppointment);
+        })
+
     
-    
-
-
-
-//Save button times
-//text input
 
 }) 
 
